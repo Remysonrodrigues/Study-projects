264562192c773006015473b74646f110d1e4317f
Remyson Costa <remysonrodrigues@gmail.com>
2020-05-18 18:16:47 -0300

    add validation

--- knowledge/backend/migrations/20200518153023_create_table_categories.js
+++ knowledge/backend/migrations/20200518153023_create_table_categories.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('categories', table => {
         table.increments('id').primary();
         table.string('name').notNull();
-        //table.integer('parentId').references('categories.id');
+        table.integer('parentId').references('categories.id');
     });
 };
 
