dda3c73dd60ce68b44162b37431b38d2b8d85e3e
Remyson Costa <remysonrodrigues@gmail.com>
2020-07-09 22:18:41 -0300

    add project auth-api

---
+++ API-Auth-JWT-TypeScript-TypeORM/src/database/migrations/1594338341400-CreateUsersTable.ts
@@ -0,0 +1,31 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUsersTable1594338341400 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable( new Table({
+            name: 'users',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'uuid',
+                    isPrimary: true,
+                    generationStrategy: 'uuid'
+                },
+                {
+                    name: 'email',
+                    type: 'varchar',
+                    isUnique: true
+                },
+                {
+                    name: 'password',
+                    type: 'varchar'                    
+                }
+            ]
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
+    }
+
+}
